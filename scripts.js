d759f740905dc9a05917d697b71de85427d241b8
Tim Heck <tih99924@bethel.edu>
2018-05-07 13:03:34 -0500

    Added GREASYSAUCEBOSS floating letters, nav bar, off canvas side nav, and some breakpoints.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,37 @@
 $(window).mousemove(function(event) {
-  var translateX = -((event.pageX - 1000) * 0.1);
-  var translateY = -((event.pageY - 1000) * 0.1);
+  var translateX = -((event.pageX - 1000) * 0.3);
+  var translateY = -((event.pageY - 1000) * 0.3);
   $("#block").css({
     "transform": "translate(" + translateX + "px, " + translateY + "px)"
   });
 });
+
+function openSideNav() {
+  $("#offCanvasNav").css({
+    "width": "250px",
+    "border-left": "5px solid #000000"
+  });
+  $("#grayBlock").css({
+    "display": "block"
+  });
+}
+
+function closeSideNav() {
+  $("#offCanvasNav").css({
+    "width": "0px",
+    "border": "none"
+  });
+  $("#grayBlock").css({
+    "display": "none"
+  });
+}
+
+function grayBlockDisplay() {
+  $("#offCanvasNav").css({
+    "width": "0px",
+    "border": "none"
+  });
+  $("#grayBlock").css({
+    "display": "none"
+  });
+}
